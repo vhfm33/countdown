7a4d5c034dd26d42aa304bc2a0ffdc8fa615536b
vitor hugo <vhfm33@gmail.com>
2015-07-20 09:20:52 -0300

    Add new images

--- app/assets/javascripts/flash.js
+++ app/assets/javascripts/flash.js
@@ -4,6 +4,27 @@ jQuery.fn.flash = function(className, timeout) {
         self.addClass(className);
         setTimeout(function() {
             self.removeClass(className);
+            var className = $('#container-gif').prop('class');
+            var indexGf = className.substr(2,1);
+            
+            console.log('index: ' + parseInt(indexGf) );
+            
+            switch(parseInt(indexGf)) {
+                case 0:
+                    $('#container-gif').removeClass('bg0').addClass('bg1');
+                    break;
+                case 1:
+                    $('#container-gif').removeClass('bg1').addClass('bg2');
+                    break;
+                case 2:
+                    $('#container-gif').removeClass('bg2').addClass('bg3');
+                    break;
+                case 3:
+                    $('#container-gif').removeClass('bg3').addClass('bg0');
+                    break;
+                default:
+                    $('#container-gif').attr('class', '').addClass('bg0');
+            }
         }, timeout);
     });
 };
